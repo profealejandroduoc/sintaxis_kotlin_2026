b5722af7b3fbddd315d49346e50cfb185a9d4210
El profe <al.sepulvedam@profesor.duoc.cl>
2026-08-21 09:47:45 -0400

    Funciones ok

---
+++ src/main/kotlin/sintaxisbasica/Funciones.kt
@@ -0,0 +1,16 @@
+package dev.movil.sintaxisbasica
+
+fun main(){
+    println("La suma es ${suma(5,10)}")
+
+    print(sum(5,5))
+}
+
+//def suma(a:int,b:int)->int:
+fun suma(a: Int, b: Int): Int {
+    return a + b
+}
+
+fun sum(a:Int,b:Int)=a+b
+
+
